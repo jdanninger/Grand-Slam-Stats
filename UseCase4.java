187fdf4a3ecc786fa970abb5f87f9c5820ce9e10
Chan Josh Kang <cxk481@case.edu>
2024-04-20 15:57:10 -0400

    Update UseCase4.java

--- UseCase4.java
+++ UseCase4.java
@@ -6,7 +6,6 @@ public class UseCase4 extends JFrame {
         JFrame frame = new JFrame(); // creates Jframe (window)
         frame.setSize(420, 420); // sets window size (x, y)
         frame.setTitle("Add/Remove/Modify Teams and Their Arena"); // sets jframe title
-
         System.out.println("We are at user case 4");
         frame.setVisible(true); //shows window
     }
